4ae653307b9f689b2ae168aebd6330722ba782a0
svyatik44 <ukrainecsvatoslav85@gmail.com>
2022-09-26 19:11:04 +0300

    step 5

--- src/Differ.php
+++ src/Differ.php
@@ -2,26 +2,10 @@
 
 namespace Differ\Differ;
 
-function buildTree(array $firstArray, array $secondArray)
-{
-    $keys = array_unique(array_merge(array_keys($firstArray), array_keys($secondArray)));
-    sort($keys);
-    $map = array_map(function ($key) use ($firstArray, $secondArray) {
-        if (!array_key_exists($key, $firstArray)) {
-            return ['key' => $key, 'value' => $secondArray[$key], 'type' => '+'];
-        }
-        if (!array_key_exists($key, $secondArray)) {
-            return ['key' => $key, 'value' => $firstArray[$key], 'type' => '-'];
-        }
-        if ($firstArray[$key] == $secondArray[$key]) {
-            return ['key' => $key, 'value' => $firstArray[$key], 'type' => 'unchanged'];
-        }
-        return ['key' => $key, 'oldValue' => $firstArray[$key],
-                'newValue' => $secondArray[$key], 'type' => '-+'];
-    }, $keys);
+use Symfony\Component\Yaml\Yaml;
 
-    return $map;
-}
+use function Parser\Parse;
+use function BuildTree\buildTree;
 
 function toString($array)
 {
@@ -29,7 +13,7 @@ function toString($array)
     foreach ($array as $value) {
         $res .= $value;
     }
-    return $res;
+    return "{\n$res}\n";
 }
 
 function showBool($value)
@@ -43,11 +27,8 @@ function showBool($value)
 
 function gendiff(string $pathToFile1, string $pathToFile2)
 {
-    $decode1 = json_decode(file_get_contents($pathToFile1));
-    $decode2 = json_decode(file_get_contents($pathToFile2));
-
-    $firstArray = (array)$decode1;
-    $secondArray = (array)$decode2;
+    $firstArray = Parse(pathinfo($pathToFile1, PATHINFO_EXTENSION), file_get_contents($pathToFile1));
+    $secondArray = Parse(pathinfo($pathToFile2, PATHINFO_EXTENSION), file_get_contents($pathToFile2));
 
     $tree = buildTree($firstArray, $secondArray);
 
@@ -55,26 +36,26 @@ function gendiff(string $pathToFile1, string $pathToFile2)
         switch ($node['type']) {
             case '+':
                 $value = showBool($node['value']);
-                return "  + {$node['key']} : {$value}\n";
+                return "  + {$node['key']}: {$value}\n";
                 break;
             case '-':
                 $value = showBool($node['value']);
-                return "  - {$node['key']} : {$value}\n";
+                return "  - {$node['key']}: {$value}\n";
                 break;
             case 'unchanged':
                 $value = showBool($node['value']);
-                return "    {$node['key']} : {$value}\n";
+                return "    {$node['key']}: {$value}\n";
                 break;
             case '-+':
                 $newValue = showBool($node['newValue']);
                 $oldValue = showBool($node['oldValue']);
-                return "  - {$node['key']} : {$oldValue}\n  + {$node['key']} : {$newValue}\n";
+                return "  - {$node['key']}: {$oldValue}\n  + {$node['key']}: {$newValue}\n";
             default:
                 print_r("error, default case\n");
                 break;
         }
     }, $tree);
-    $string = toString($list);
-    $result = "{\n$string}\n";
+
+    $result = toString($list);
     return $result;
 }
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -12,5 +12,8 @@ class DifferTest extends TestCase
     {
         $expected = file_get_contents("tests/fixtures/SucsessDiffer.txt");
         $this->assertEquals($expected, gendiff("tests/fixtures/file1.json", "tests/fixtures/file2.json"));
+
+        $expected2 = file_get_contents("tests/fixtures/SucsessDifferYaml.txt");
+        $this->assertEquals($expected2, gendiff("tests/fixtures/file1.yml", "tests/fixtures/file2.yml"));
     }
 }
