c20438676ace05ec0f29a2593b4a7d3d97d39ba1
svyatik44 <ukrainecsvatoslav85@gmail.com>
2022-09-27 19:03:22 +0300

    fix filename Parser

---
+++ src/Parser.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BuildTree;
+
+function buildTree(array $firstArray, array $secondArray)
+{
+    $keys = array_unique(array_merge(array_keys($firstArray), array_keys($secondArray)));
+    sort($keys);
+    $map = array_map(function ($key) use ($firstArray, $secondArray) {
+        if (!array_key_exists($key, $firstArray)) {
+            return ['key' => $key, 'value' => $secondArray[$key], 'type' => '+'];
+        }
+        if (!array_key_exists($key, $secondArray)) {
+            return ['key' => $key, 'value' => $firstArray[$key], 'type' => '-'];
+        }
+        if ($firstArray[$key] == $secondArray[$key]) {
+            return ['key' => $key, 'value' => $firstArray[$key], 'type' => 'unchanged'];
+        }
+        return ['key' => $key, 'oldValue' => $firstArray[$key],
+                'newValue' => $secondArray[$key], 'type' => '-+'];
+    }, $keys);
+
+    return $map;
+}
