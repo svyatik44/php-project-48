3b44b8eaf2770d804c2c20fca2b2076e1a7bc8f8
svyatik44 <ukrainecsvatoslav85@gmail.com>
2022-11-12 15:02:58 +0300

    names & change filedir

--- src/Differ.php
+++ src/Differ.php
@@ -3,8 +3,34 @@
 namespace Differ\Differ;
 
 use function Parser\parse;
-use function BuildTree\buildTree;
 use function Format\Parser\parseFormat;
+
+use function Functional\sort;
+
+function buildTree(array $firstCollection, array $secondCollection): array
+{
+    $keys = array_unique(array_merge(array_keys($firstCollection), array_keys($secondCollection)));
+    $sortedKeys = sort($keys, fn ($left, $right) => $left <=> $right);
+
+    $map = array_map(function ($key) use ($firstCollection, $secondCollection) {
+        if (!array_key_exists($key, $firstCollection)) {
+            return ['key' => $key, 'value' => $secondCollection[$key], 'type' => '+'];
+        }
+        if (!array_key_exists($key, $secondCollection)) {
+            return ['key' => $key, 'value' => $firstCollection[$key], 'type' => '-'];
+        }
+        if ($firstCollection[$key] === $secondCollection[$key]) {
+            return ['key' => $key, 'value' => $firstCollection[$key], 'type' => 'unchanged'];
+        }
+        if (is_array($firstCollection[$key]) && is_array($secondCollection[$key])) {
+            return ['key' => $key, 'type' => 'array', 'child' => buildTree($firstCollection[$key], $secondCollection[$key])];
+        }
+        return ['key' => $key, 'oldValue' => $firstCollection[$key],
+                'newValue' => $secondCollection[$key], 'type' => '-+'];
+    }, $sortedKeys);
+
+    return $map;
+}
 
 function genDiff(string $pathToFile1, string $pathToFile2, string $format = 'stylish'): string
 {
